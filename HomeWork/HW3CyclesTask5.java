2172de4172cd10fac50a3fd7542b59bee7b0c5f2
AWUAWAOG <rimuryt200201@gmail.com>
2022-10-02 20:48:03 +0300

    Homework #3 Control Structures & Cycles

---
+++ HomeWork/HW3CyclesTask5.java
@@ -0,0 +1,13 @@
+package HomeWork;
+
+public class HW3CyclesTask5 {
+    public static void main(String[] args) {
+
+        int x = 5;
+        while (x>-45) {
+            x=x-5;
+            System.out.println(x);
+        }
+
+    }
+}
