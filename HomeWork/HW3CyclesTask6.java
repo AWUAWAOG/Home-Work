705e98b24cfc474f3c4f893033cf6806e065eb9d
AWUAWAOG <rimuryt200201@gmail.com>
2022-10-02 20:52:46 +0300

    Homework #3 Control Structures & Cycles

---
+++ HomeWork/HW3CyclesTask6.java
@@ -0,0 +1,19 @@
+package HomeWork;
+
+public class HW3CyclesTask6 {
+    public static void main(String[] args) {
+
+        System.out.println(Math.pow(10,2));
+        System.out.println(Math.pow(11,2));
+        System.out.println(Math.pow(12,2));
+        System.out.println(Math.pow(13,2));
+        System.out.println(Math.pow(14,2));
+        System.out.println(Math.pow(15,2));
+        System.out.println(Math.pow(16,2));
+        System.out.println(Math.pow(17,2));
+        System.out.println(Math.pow(18,2));
+        System.out.println(Math.pow(19,2));
+        System.out.println(Math.pow(20,2));
+
+    }
+}
