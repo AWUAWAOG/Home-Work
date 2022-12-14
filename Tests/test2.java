1c66ba0c6bd800fc73995c7ba13a6ec079fdb43c
AWUAWAOG <rimuryt200201@gmail.com>
2022-12-15 00:29:59 +0300

    test

---
+++ Tests/test2.java
@@ -0,0 +1,15 @@
+package Tests;
+
+import java.util.concurrent.Callable;
+
+public class test2 {
+    public static void main(String[] args) {
+        Callable<Object> callable = new Callable<Object>() {
+            public Object call() throws Exception {
+
+                return null;
+            }
+        };
+    }
+
+}
